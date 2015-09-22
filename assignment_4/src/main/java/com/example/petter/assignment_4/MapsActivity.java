f68a4e24e8283025f929411f047c22f9ef6aa28e
Peethaa <petter_olsson@hotmail.com>
2015-09-22 14:14:42 +0200

    Update 4: improvment for Assignment 4

--- assignment_4/src/main/java/com/example/petter/assignment_4/MapsActivity.java
+++ assignment_4/src/main/java/com/example/petter/assignment_4/MapsActivity.java
@@ -1,22 +1,49 @@
 package com.example.petter.assignment_4;
 
+import android.app.FragmentTransaction;
+import android.location.Location;
+import android.location.LocationListener;
+import android.nfc.Tag;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+
+//Smthn Smthn Dark Side. Glöm inte att ändra fragment för fT
+public class MapsActivity extends FragmentActivity implements
+        GoogleMap.OnMapClickListener,
+        GoogleApiClient.ConnectionCallbacks,
+        com.google.android.gms.location.LocationListener{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "MapsActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API),
+                .addConnectionCallbacks(this),
+                .build();
     }
 
     @Override
@@ -63,8 +90,79 @@ public class MapsActivity extends FragmentActivity {
      */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(55.600500, 12.979994)).title("Hemma"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(55.600500, 12.979994)).title("Hemma"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(50.600500, 12.979994)).title("Hemma"));
+
+        mMap.setMyLocationEnabled(true); //Här är jag
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID); //Karttyp
+
+        //To make a mockup location:
+        //Download telenet
+        //CMD --> telnet localhost
+        //geo fix 55.6 12.97
+
+        UiSettings mUISettings = mMap.getUiSettings();
+        mUISettings.setZoomControlsEnabled(true);
+        mUISettings.setMyLocationButtonEnabled(true);
+
+        LatLng home = new LatLng(55.61, 12.97); //Anger en LatLng kordinat
+        LatLng work = new LatLng(57.61, 15.97);
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(home));//Flyttar kameran till kordinaten
+        //mMap.moveCamera(CameraUpdateFactory.zoomTo(16));//Zooma in 1-20
+
+        Location myLocation = mMap.getMyLocation();//Hämtar nuvarande plats
+        myLocation.getTime(); //Många alternativ
+
+
+        //To add more markers copy and paste below:
+
+
+        mMap.addMarker(new MarkerOptions() //Mycket expansionsvänlig
+                        .position(home)
+                        .title("Hemma")
+                        .snippet("This is sparta!")
+                //För att ändra ikon på kartan. ie. florist, cafe, etc.
+                //.icon(BitmapDescriptorFactory.fromResource(R.drawable.Något))
+        );
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(home));//Flyttar kameran till kordinaten
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(16));//Zooma in 1-20
+
+        mMap.setOnMarkerClickListener((GoogleMap.OnMarkerClickListener) this); //Adjust to fit wtih existing marker
 
     }
+
+    @Override
+    public void onMapClick(LatLng latLng) { //(Marker marker)
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        MarkerClickDialog mDialog = new MarkerClickDialog();
+
+        mDialog.show(ft,"dialog");
+
+        return true;
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Connected");
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG,"Location" +location.getLongitude()+"time"+ getTime);
+    }
+
+
 }
+
+
---
+++ assignment_4/src/main/java/com/example/petter/assignment_4/MarkerClickDialog.java
@@ -0,0 +1,52 @@
+package com.example.petter.assignment_4;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by PeethTMG on 22-09-2015.
+ */
+public class MarkerClickDialog extends DialogFragment implements Dialog.OnClickListener {
+
+
+    public MarkerClickDialog() {
+
+    }
+    //Dialog från klick på kartan.
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity())
+                .setTitle("Cafe")
+                .setMessage("This Cafe is the best Cafe!")
+                .setPositiveButton("OK", this)
+                .setNegativeButton("Cancel", this);
+
+        return dialog.create();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case Dialog.BUTTON_POSITIVE:
+                break;
+        }
+    }
+}
